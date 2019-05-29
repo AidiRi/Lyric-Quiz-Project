a9f1b175ae5bdc6a286ed2bc29278d7d6045809c
AidiRi <rivera.aidi@gmail.com>
2019-05-29 08:42:12 -0700

    added #present_menu

--- app/models/CLI-lyrics-quiz.rb
+++ app/models/CLI-lyrics-quiz.rb
@@ -3,6 +3,23 @@ class CLILyricsQuiz
     end
 
     def run
-      puts "Welcome to the Lyrics Quiz CLI!"
+      puts
+      puts
+      puts "Welcome to the Lyrics Quiz CLI!".center(50)
+      puts "*****************".center(50)
+      puts
+      present_menu
+      puts
+      puts
+
+    end
+
+    def present_menu
+      puts "What would you like to do?"
+      puts
+      puts "1. Play a New Game"
+      puts "2. Check High Scores"
+      puts "3. How to Play"
+      puts "4. Quit"
     end
 end
