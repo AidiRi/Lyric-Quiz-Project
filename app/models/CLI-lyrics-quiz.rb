776cedbc67053ebcde4dae3b5b7ea61bcd9a4ed2
AidiRi <rivera.aidi@gmail.com>
2019-05-29 09:08:31 -0700

    added #run logic and #how_to

--- app/models/CLI-lyrics-quiz.rb
+++ app/models/CLI-lyrics-quiz.rb
@@ -8,10 +8,24 @@ class CLILyricsQuiz
       puts "Welcome to the Lyrics Quiz CLI!".center(50)
       puts "*****************".center(50)
       puts
-      present_menu
+
       puts
       puts
 
+      is_running = true
+      while is_running
+        present_menu
+        choice = STDIN.gets.chomp.to_i
+        if choice == 1
+          # play game
+        elsif choice == 2
+          # go to highscores
+        elsif choice == 3
+          how_to
+        elsif choice == 4
+          is_running = false
+        end
+      end
     end
 
     def present_menu
@@ -21,5 +35,18 @@ class CLILyricsQuiz
       puts "2. Check High Scores"
       puts "3. How to Play"
       puts "4. Quit"
+      puts
+    end
+
+    def how_to
+      puts
+      puts
+      puts "How To Play".center(50)
+      puts "*****************".center(50)
+      puts
+      puts "howtohowtohowtohowtohowto".center(50)
+      puts
+      puts"*****************".center(50)
+      puts
     end
 end
